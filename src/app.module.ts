4e0fa8bbdad7299e97d65147253327c96f00b336
tjaos <th83287716@gmail.com>
2024-11-25 14:52:18 -0300

    feat: criação das entidades + importação no app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RidesModule } from './rides/rides.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RidesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_NAME || 'rides_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),    
+    RidesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/rides/entities/ride.entity.ts
+++ src/rides/entities/ride.entity.ts
@@ -1,1 +1,33 @@
-export class Ride {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Customer } from './customer.entity';
+import { Driver } from './driver.entity';
+
+@Entity()
+export class Ride {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Customer, (customer) => customer.id)
+  customer: Customer;
+
+  @ManyToOne(() => Driver, (driver) => driver.id)
+  driver: Driver;
+
+  @Column()
+  origin: string;
+
+  @Column()
+  destination: string;
+
+  @Column('decimal')
+  distance: number;
+
+  @Column()
+  duration: string;
+
+  @Column('decimal')
+  value: number;
+
+  @Column()
+  date: Date;
+}
