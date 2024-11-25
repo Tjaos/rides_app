f093a1c2217b113462c80853c1575cd4e9e43092
tjaos <th83287716@gmail.com>
2024-11-25 15:25:17 -0300

    feat:criação do serviço que usará a api do google maps

---
+++ src/rides/googleMaps.service.ts
@@ -0,0 +1,25 @@
+import { HttpService } from "@nestjs/axios";
+import { Injectable} from "@nestjs/common/";
+
+@Injectable()
+export class GoogleMapsService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getRoute(origin: string, destination: string) {
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}`;
+
+    const response = await this.httpService.get(url).toPromise();
+    if (response.status !== 200) {
+      throw new Error('Failed to fetch route');
+    }
+
+    const data = response.data.routes[0].legs[0];
+    return {
+      distance: data.distance.value / 1000,
+      duration: data.duration.text,
+      start_location: data.start_location,
+      end_location: data.end_location,
+    };
+  }
+}
--- src/rides/rides.module.ts
+++ src/rides/rides.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RidesService } from './rides.service';
 import { RidesController } from './rides.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [RidesController],
   providers: [RidesService],
 })
